83fba471f557dd1cf1bf0c80d2666121185bef48
GribanovDS <74065724+GribanovDS@users.noreply.github.com>
2023-04-04 14:01:50 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,8 +5,8 @@ import numpy as np
 chat_id = 1219503064 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    time = 93
-    distances = x.copy()
-    distances /= ((93**2)/2)
-    answer = distances.mean()
+    time = 93 #время по условию
+    distances = x.copy() #массив с расстояниями
+    distances /= ((time**2)/2) #массив с коэффициентами ускорений
+    answer = distances.mean() #точечная оценка коэффициента ускорения
     return answer
